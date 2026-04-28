fdabcb72bda2289c4c6c3c4318e6e9ccf10fc368
𐰀𐰖𐰋𐰅𐰚 <heyaibek@gmail.com>
2026-04-28 17:49:19 +0200

    inject app and os versions into mail form

--- app/support/page.tsx
+++ app/support/page.tsx
@@ -8,12 +8,37 @@ export const metadata: Metadata = {
   },
 };
 
-export default function SupportPage() {
+type SupportPageProps = {
+  searchParams: Promise<{
+    app_version?: string;
+    os_version?: string;
+  }>;
+};
+
+export default async function SupportPage({ searchParams }: SupportPageProps) {
+  const params = await searchParams;
+  const appVersion = params.app_version;
+  const osVersion = params.os_version;
+  let bugReportLink =
+    "mailto:contact@moonkata.com?subject=Bug%20Report%20for%20Vibely%20-%20Music%20Visualizer&body=What%E2%80%99s%20not%20working%20in%20Vibely%20for%20you%3F%20Be%20specific%20and%20provide%20examples%20(attach%20screenshots%20or%20screen%20recordings).%20Steps%20to%20reproduce%20are%20welcomed%20%F0%9F%99%8F%F0%9F%8F%BB%0D%0A%0D%0ATYPE_HERE%0D%0A%0D%0AApp%20version%3A%20APP_VERSION%0D%0AOS%20version%3A%20OS_VERSION%0D%0A";
+  if (appVersion) {
+    bugReportLink = bugReportLink.replaceAll("APP_VERSION", appVersion);
+  }
+  if (osVersion) {
+    bugReportLink = bugReportLink.replaceAll("OS_VERSION", osVersion);
+  }
   return (
     <div className="flex flex-col gap-6 text-zinc-900 dark:text-zinc-100">
       <h1 className="text-3xl font-semibold leading-10 tracking-tight">
         Support
       </h1>
+      {appVersion && osVersion && (
+        <p>
+          App version: {appVersion}
+          <br />
+          OS version: {osVersion}
+        </p>
+      )}
       <p className="max-w-2xl text-base leading-7 text-zinc-600 dark:text-zinc-400">
         Need help with Vibely? We are here for you.
       </p>
@@ -24,17 +49,17 @@ export default function SupportPage() {
         <li>
           <a
             className="underline underline-offset-4 hover:opacity-80"
-            href="mailto:contact@moonkata.com?subject=Feature%20Request%20for%20Vibely%20-%20Music%20Visualizer&body=What%20feature%20you%20would%20like%20to%20request%3F%20Be%20specific%20and%20provide%20examples%0D%0A%0D%0A___%0D%0A%0D%0AAttach%20example%20screenshots%20or%20screen%20recordings%20or%20YouTube%2C%20TikTok%2C%20Instagram%20links%0D%0A"
+            href="mailto:contact@moonkata.com?subject=Feature%20Request%20for%20Vibely%20-%20Music%20Visualizer&body=What%20feature%20you%20would%20like%20to%20request%3F%20Be%20specific%20and%20provide%20examples%3A%0D%0A%0D%0ATYPE_HERE%0D%0A%0D%0AAttach%20example%20screenshots%20or%20screen%20recordings%20or%20YouTube%2C%20TikTok%2C%20Instagram%20links%0D%0A"
           >
-            Feature request email
+            Feature request
           </a>
         </li>
         <li>
           <a
             className="underline underline-offset-4 hover:opacity-80"
-            href="mailto:contact@moonkata.com?subject=Bug%20Report%20for%20Vibely%20-%20Music%20Visualizer&body=What%E2%80%99s%20not%20working%20in%20Vibely%20for%20you%3F%20Be%20specific%20and%20provide%20examples.%20Steps%20to%20reproduce%20are%20welcomed%20%F0%9F%99%8F%F0%9F%8F%BB%0D%0A%0D%0A___%0D%0A%0D%0AApp%20version%20(For%20example%20v3.8.6)%3A%0D%0A%0D%0A___%0D%0A%0D%0AOS%20version%20(For%20example%20iOS%2026.3)%3A%0D%0A%0D%0A___%0D%0A%0D%0AAttach%20screenshots%20or%20screen%20recordings"
+            href={bugReportLink}
           >
-            Bug report email
+            Bug report
           </a>
         </li>
         <li>
@@ -42,7 +67,7 @@ export default function SupportPage() {
             className="underline underline-offset-4 hover:opacity-80"
             href="mailto:contact@moonkata.com?subject=General%20Question%20about%20Vibely%20-%20Music%20Visualizer&body=How%20can%20we%20help%20you%3F%0D%0A%0D%0A___%0D%0A%0D%0APlease%20include%20any%20helpful%20details%2C%20screenshots%2C%20or%20links."
           >
-            General support email
+            General support
           </a>
         </li>
       </ul>
